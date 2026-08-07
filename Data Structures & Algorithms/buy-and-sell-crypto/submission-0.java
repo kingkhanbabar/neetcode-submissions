723f3896cc323fdb3fcfdc77f643e621c4c68808
SHAHVEJ KHAN <shahvej2506328@st.jmi.ac.in>
2026-08-07 15:58:32 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit =0;
+        int n = prices.length;
+        for(int i=0;i<n;i++){
+            if(prices[i]<minPrice){
+                minPrice = prices[i];
+            }
+            else{
+                maxProfit = Math.max(maxProfit,prices[i]-minPrice);
+            }
+        }
+        return maxProfit;
+    }
+}
